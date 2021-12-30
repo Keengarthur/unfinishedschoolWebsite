0c6ced4a3bc24f65c7295868474f6ae3bfbabda1
Keengarthur <arthurkeengs@gmail.com>
2021-12-30 00:55:32 -0800

    mongo url visible

--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-// const url ='mongodb+srv://keeng:keeng@keengarthur.sum6w.mongodb.net/student?retryWrites=true&w=majority'
 
 const connectDb = (url) =>{
     return mongoose.connect(url)
